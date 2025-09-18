7f2ae69275901826dd0e4487a461064ccea6a587
LSH005 <isihyeon005shingu@gmail.com>
2025-09-18 16:13:00 +0900

    3차시

--- GEA/Assets/PlayerController.cs
+++ GEA/Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,24 +8,84 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public float jumpPower = 5f;
     public float gravity = -9.81f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+
+    private float currentSpeed;
+
+
+    public CinemachineVirtualCamera virtualCam;
+    public float rotationSpeed = 10f;
+    private CinemachinePOV pov;
     private CharacterController controller;
     private Vector3 velocity;
     public bool isGrounded;
+
+    public CinemacineSwitcher cs;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        pov = virtualCam.GetCinemachineComponent<CinemachinePOV>();
+
+        currentSpeed = walkSpeed;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = runSpeed;
+
+            // 카메라 FOV 확대 효과
+            if (virtualCam != null)
+            {
+                float targetFOV = 70f;
+                virtualCam.m_Lens.FieldOfView = Mathf.Lerp(virtualCam.m_Lens.FieldOfView, targetFOV,Time.deltaTime * 5f);
+
+
+            }
+
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+
+            if (virtualCam != null)
+            {
+                float targetFOV = 60f;
+                virtualCam.m_Lens.FieldOfView,targetFOV,Time.deltaTime * 5f);
+            }
+
+
+        }
+
         isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(x, 0, z);
+        Vector3 camForward = virtualCam.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        Vector3 camRight = virtualCam.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 move = (camForward * z + camRight * x).normalized;
+
+        if(!cs.usingFreeLook)
         controller.Move(move * speed * Time.deltaTime);
+
+        float cameraYaw = pov.m_HorizontalAxis.Value;
+        Quaternion targetRot = Quaternion.Euler(0f, cameraYaw, 0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
